c8eb4c93e9d00d9e0040cc4c8947907180620294
Equinoxe-fgc <31983490+Equinoxe-fgc@users.noreply.github.com>
2017-10-19 11:32:07 +0200

    Información de pulsaciones

--- app/src/main/java/com/equinoxe/myapplicationpantallatactil/MainActivity.java
+++ app/src/main/java/com/equinoxe/myapplicationpantallatactil/MainActivity.java
@@ -2,12 +2,26 @@ package com.equinoxe.myapplicationpantallatactil;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView entrada = (TextView)findViewById(R.id.textViewEntrada);
+        entrada.setOnTouchListener(this);
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        TextView salida = (TextView)findViewById(R.id.textViewSalida);
+        salida.append(motionEvent.toString() + "\n");
+
+        return true;
     }
 }
